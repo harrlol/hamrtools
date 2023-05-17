7281e659d22ec1fc70c374c125bd3932e47c8ce8
Harry Li <87460010+harrlol@users.noreply.github.com>
2023-05-17 16:55:45 -0400

    bridges the steps to and in R

---
+++ allLapPrep.R
@@ -0,0 +1,82 @@
+library(data.table)
+library(tidyr)
+library(dplyr)
+
+args=commandArgs(trailingOnly=TRUE)
+
+lap.clean <- function(bed, type) {
+  if (type=="gene") {
+    out <- select(bed, seq=V7, pos=V8, mod=V10, gene=V4, score=V11, strand=V12)
+  } else if (type=="UTR") {
+    out <- select(bed, seq=V7, pos=V8, mod=V10, gene=V5, feature=V6, score=V11, strand=V12)
+  } else if (type=="CDS") {
+    out <- select(bed, seq=V6, pos=V7, mod=V9, gene=V5, score=V10, strand=V11)
+  } else if (type=="mRNA") {
+    out <- select(bed, seq=V7, pos=V8, mod=V10, gene=V4, score=V11, strand=V12)
+  } else {stop("invalid type")}
+  return(out)
+}
+
+isoUnsensitive <- function (modtbl) {
+  modtbl%>%
+    group_by(across(c(-gene)))%>%
+    summarize(gene = unique(unlist(strsplit(as.character(gene),".", fixed = TRUE))[1]))
+}
+
+allLapPrep <- function(dir) {
+  file_names <- list.files(path = dir)
+  
+  # Initialize long df
+  longdf <- NULL
+  for (file_name in file_names) {
+    
+    # First split the file 
+    finfo <- unlist(strsplit(tools::file_path_sans_ext(file_name), split = "_", fixed=TRUE))
+    
+    # Obtain the type of overlap of current file (UTR, mRNA, CDS, gene)
+    lap_type <- finfo[length(finfo)]
+    
+    # Obtain the sequencing technique (mRNA seq, GMUCT, NAD, etc.)
+    seq_tech <- finfo[length(finfo) - 1]
+    
+    # Obtain the genotype of the sample group
+    genotype <- paste(setdiff(finfo, c(lap_type, seq_tech)), collapse = "_")
+    
+    # Import the file as a variable and pipe through the cleaning steps 
+    temp <- assign(file_name, fread(file.path(dir, file_name), stringsAsFactors = TRUE))
+    temp_clean <- lap.clean(temp, lap_type)
+    
+    # UTR needs a bit more work because 5' and 3' isoform cleaning
+    if (lap_type == "UTR") {
+      # Add experimental information alongside hamr predictions and bind to long df
+      to_add1 <- data.frame(isoUnsensitive(temp_clean))%>%
+        filter(feature=="three_prime_UTR")%>%
+        mutate(genotype=genotype) %>%
+        mutate(seq_tech=seq_tech) %>%
+        mutate(lap_type="3'UTR") %>%
+        select(-feature)
+      to_add2 <- data.frame(isoUnsensitive(temp_clean))%>%
+        filter(feature=="five_prime_UTR")%>%
+        mutate(genotype=genotype) %>%
+        mutate(seq_tech=seq_tech) %>%
+        mutate(lap_type="5'UTR") %>%
+        select(-feature)
+      longdf <- rbind(longdf, to_add1, to_add2)
+    } else if (lap_type == "CDS") {
+      to_add <- data.frame(isoUnsensitive(temp_clean))%>%
+        mutate(genotype=genotype) %>%
+        mutate(seq_tech=seq_tech) %>%
+        mutate(lap_type=lap_type)
+      longdf <- rbind(longdf, to_add)
+    } else {
+      to_add <- data.frame(temp_clean)%>%
+        mutate(genotype=genotype) %>%
+        mutate(seq_tech=seq_tech) %>%
+        mutate(lap_type=lap_type)
+      longdf <- rbind(longdf, to_add)
+    }
+  }
+  return(longdf)
+}
+
+project1 <- allLapPrep(args[1])
---
+++ findConsensus.R
@@ -0,0 +1,80 @@
+library(data.table)
+library(tidyr)
+library(dplyr)
+
+args=commandArgs(trailingOnly=TRUE)
+
+bed2modtbl <- function(bed) {
+  out <- bed%>%
+    mutate(start=bp, end =bp, seqname=chr, score=".", Name=as.character(pred.mod), strand=as.character(strand))%>%
+    select(seqname, start, end, Name, score, strand)
+  return(out)
+}
+
+comb.intersect <- function(bed1, bed2) {
+  bed1.mods = bed2modtbl(bed1)
+  bed2.mods = bed2modtbl(bed2)
+  consensus <- intersect(bed1.mods, bed2.mods)
+  return(consensus)
+}
+
+comb.union2 <- function(bedlist) {
+  #takes the first bed to be the basis of future extensions
+  out <- data.frame(bedlist[[1]])
+  #for each bed after the first one, find the difference between it and what's in out already, and add to out
+  for (bed in bedlist[-1]) {
+    temp <- data.frame(bed)
+    new <- setdiff(temp, out)
+    out <- rbind(out, new)
+  }
+  return(out)
+}
+
+findConsensus <- function(dir) {
+  file_names <- list.files(path = dir)
+  processed_variables <- c()
+  for (file_name in file_names) {
+    if (!(file_name %in% processed_variables)) {
+      # Extract the common part of the variable name
+      common_part <- sub("_[^_]*$", "", file_name)
+      # Find variables with the same common part
+      variables_to_process <- grep(paste0("^", common_part, "_"), file_names, value = TRUE)
+      
+      # Add the selected files to processed variables
+      processed_variables <<- c(processed_variables, variables_to_process)
+      
+      # If only 1 rep, then that rep is the consensus
+      if (length(variables_to_process)==1) {
+        consensus <- bed2modtbl(fread(file.path(dir, variables_to_process)))
+        write.table(consensus, paste0(dir, "/", common_part, ".bed"), sep='\t', row.names=F, col.names=F, quote=F)
+      } else {
+        # If >1 rep, find combinations
+        # Generate a matrix of 2 combinations of the files
+        variable_combinations <- combn(variables_to_process, 2)
+        
+        # Initiate list to contain all intersections
+        intersect_list <- list()
+        for (i in 1:ncol(variable_combinations)) {
+          var1 <- assign(variable_combinations[1, i], fread(file.path(dir, variable_combinations[1, i]))) 
+          var2 <- assign(variable_combinations[2, i], fread(file.path(dir, variable_combinations[2, i])))
+          output <- comb.intersect(var1, var2)
+          output_name <- paste0(variable_combinations[1, i], "_", variable_combinations[2, i])
+          assign(output_name, output, envir = .GlobalEnv)
+          intersect_list[[output_name]] <- output
+        }
+        
+        # If only 1 intersect (only 2 reps), then that intersect is the consensus
+        if (length(intersect_list)==1) {
+          write.table(intersect_list, paste0(dir, "/", common_part, ".bed"), sep='\t', row.names=F, col.names=F, quote=F)
+        } else {
+          # If >2 rep, then union is taken 
+          # Apply union to the list of intersections
+          consensus <- comb.union2(intersect_list)
+          write.table(consensus, paste0(dir, "/", common_part, ".bed"), sep='\t', row.names=F, col.names=F, quote=F)
+        }
+      }
+    }
+  }
+}
+
+findConsensus(args[1])
