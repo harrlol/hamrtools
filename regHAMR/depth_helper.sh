62abc6b80cb699e790a319cd187563599f1961d6
Harry Li <87460010+harrlol@users.noreply.github.com>
2023-06-22 17:30:12 -0400

    new depth analysis step and code cleaning

---
+++ regHAMR/depth_helper.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -u
+
+# Script to use sratool (newest version) that is otherwise unavailable in bash 
+
+# First, install the relative sratoolkit manually from https://github.com/ncbi/sra-tools/wiki/01.-Downloading-SRA-Toolkit 
+
+# Next, create an SRR accession list (in .txt format) using the SRA run selector tool.
+
+# Locate both your sratoolkit and your .txt
+
+
+if [ "$#" -lt 5 ]; then
+echo "Missing arguments!"
+echo "EXAMPLE:"
+exit 1
+fi
+
+out=$1
+t=$2
+d=$3
+n=$4
+f=$5
+
+for ff in $out/pipeline/depth/*.bam
+    do
+        if echo "$ff" | grep -q "$n"
+        then
+            tt=$(basename $ff)
+            nn=${tt%.*}
+            echo "extracting depth information from $nn"
+            for i in $(seq 1 $(wc -l < $f))
+            do
+                chr=$(sed "${i}q;d" $f | sed 's/\t/\n/g' | sed '1q;d')
+                pos=$(sed "${i}q;d" $f | sed 's/\t/\n/g' | sed '2q;d')
+                dph=$(samtools coverage \
+                    -r $chr:$pos-$pos \
+                    $ff \
+                    | awk 'NR==2' | awk -F'\t' '{print $7}')
+                awk -v "i=$i" 'NR==i {print $0"\t"var; next} 1' var="$dph" $f > $d/${nn}_new.bed && mv $d/${nn}_new.bed $f
+            done
+            echo "finished $nn"
+        fi
+    done
--- regHAMR/fastq2hamr.sh
+++ regHAMR/fastq2hamr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -eu
+set -u
 
 # Script to process sequencing data in fastq format to apply HAMR. See 
 #HAMR: High-Throughput Annotation of Modified Ribonucleotides, Lee E. Vandivier et al. for reference. 
@@ -21,26 +21,60 @@ set -eu
 #note bowtie index needs to be prebuilt and directory available prior to running this script
 #input fastq file should be pre-trimmed of adaptors
 
-if [ "$#" -lt 8 ]; then
+if [ "$#" -lt 11 ]; then
 echo "Missing arguments!"
-echo "USAGE: fastq2hamr.sh <sample.fastq> <anotation.gtf/gff3> <bowtie dir> 
-<genome.fasta> <sorting_script.pl> <hamr_model.Rdata> <out dir> <mismatch_num> <filename dict>"
+echo "USAGE: fastq2hamr.sh <sample.fastq> <anotation.gtf/gff3> 
+<genome.fasta> <sorting_script.pl> <hamr_model.Rdata> <out dir> <mismatch_num> <filename dict> <thread num> <genome length> <repo>"
 echo "EXAMPLE:"
 exit 1
 fi
 
 smp=$1
 ant=$2
-bt=$3
-gno=$4
-sort=$5
-mdl=$6
+gno=$3
+sort=$4
+mdl=$5
 #root out, true out and hamr out assigned later
-rout=$7
-len=$8
-dic=$9
+rout=$6
+mis=$7
+ohang=$((mis-1))
+dic=$8
+cores=$9
+gnolen=${10}
+repo=${11}
 # Assume single end
 det=1
+
+#######################
+# Running checks to ensure program can run normally
+gatk=""
+hamr=""
+
+# Grab command from repo where needed
+shopt -s nocaseglob
+
+for folder in "$repo"/*; do
+    # Check if the folder name contains the string "sratoolkit"
+    if [[ "$folder" == *gatk* ]]; then
+        gatk="$folder"
+    # Check if the folder name contains the string "hamr_py3" (case unsensitive)
+    elif [[ "${folder,,}" == *hamr_py3* ]]; then
+        hamr="$folder"
+    fi
+done
+
+shopt -u nocaseglob
+
+if [ ! -n "$gatk" ]; then
+    echo "GATK not installed, please check."
+    exit 1
+fi
+
+if [ ! -n "$hamr" ]; then
+    echo "HAMR not installed, please check."
+    exit 1
+fi
+
 
 smpext=$(basename "$smp")
 smpdir=$(dirname "$smp")
@@ -65,7 +99,7 @@ else
   echo ""
 fi
 
-# Real the CSV file into a DataFrame
+# Read the CSV file into a DataFrame
 mapfile -t names < <(awk -F, '{ print $1 }' "$dic")
 mapfile -t smpf < <(awk -F, '{ print $2 }' "$dic")
 
@@ -75,7 +109,6 @@ for ((i=0; i<${#names[@]}; i++)); do
     dictionary[${names[i]}]=${smpf[i]}
 done
 
-
 if [[ $smpkey == *_trimmed* ]]; then
   smpkey="${smpkey%_trimmed*}"
 fi
@@ -84,9 +117,9 @@ fi
 if [[ ${dictionary[$smpkey]+_} ]]; then
     smpname="${dictionary[$smpkey]}"
     smpname="${smpname//$'\r'}"
-    echo "Sample Group: $smpname"
+    echo "[$smpkey] Sample group name found: $smpname"
 else
-    echo "No filename found for input: $smpkey"
+    echo "[$smpkey] Could not locate sample group name, exiting..."
     exit 1
 fi
 
@@ -94,11 +127,11 @@ fi
 if [ ! -d "$rout/pipeline/$smpkey""_temp" ] 
 then
     mkdir "$rout/pipeline/$smpkey""_temp"
-    echo "created path: $rout/pipeline/$smpkey""_temp"
+    echo "[$smpkey] created path: $rout/pipeline/$smpkey""_temp"
 fi
 
 out=$rout/pipeline/$smpkey"_temp"
-echo "You can find all the intermediate files for $smpkey at $out" 
+echo "[$smpkey] You can find all the intermediate files for $smpkey at $out" 
 
 
 # Reassign hamr output directory
@@ -109,77 +142,66 @@ then
 fi
 
 hamrout=$rout/hamr_out
-echo "You can find the HAMR output file for $smpkey at $hamrout/$smpname.mod.txt" 
+echo "[$smpkey] You can find the HAMR output file for $smpkey at $hamrout/$smpname.mod.txt" 
 
 
-echo ""
-echo "sample path = $1"
-echo "sample SRR = $smpkey"
-echo "sample name = $smpname"
-echo "annotation = $2"
-echo "bowtie directory = $3"
-echo "genome = $4"
-echo "scorting script = $5"
-echo "hamr model = $6"
-echo "mismatch number = $8"
-echo ""
-
-
-red=8
-if (($len > 8)); then
-  red=$((len +1))
-fi
-
+echo "[$smpkey] Begin HAMR pipeline"
+cd "$rout/pipeline/$smpkey""_temp"
 #maps the trimmed reads to provided annotated genome, can take ~1.5hr
 #fine tuning of the arguments might be needed
 if [ "$det" -eq 1 ]; then
-  echo "Performing tophat2 with a single-end file."
-  tophat2 --library-type fr-firststrand --read-mismatches $len --read-edit-dist $red  --max-multihits 10 \
-    --b2-very-sensitive \
-    --transcriptome-max-hits 10 \
-    --no-coverage-search \
-    --output-dir $out \
-    -G $ant \
-    -p 4 \
-    $bt \
-    $smp
-
+  echo "[$smpkey] Performing STAR with a single-end file."
+  STAR \
+    --runThreadN $cores \
+    --genomeDir $rout/ref \
+    --readFilesIn $smp \
+    --sjdbOverhang $ohang \
+    --sjdbGTFfile $ant \
+    --outFilterMultimapNmax 10 \
+    --outFilterMismatchNmax $mis \
+    --outSAMtype BAM SortedByCoordinate
 else
-  echo "Performing tophat2 with a paired-end file."
-  tophat2 --library-type fr-firststrand --read-mismatches $len --read-edit-dist $red --max-multihits 10 \
-    --b2-very-sensitive \
-    --transcriptome-max-hits 10 \
-    --no-coverage-search \
-    --output-dir $out \
-    -G $ant \
-    -p 4 \
-    $bt \
-    $smp1 $smp2
+  echo "[$smpkey] Performing STAR with a paired-end file."
+  STAR \
+    --runThreadN $cores \
+    --genomeDir $rout/ref \
+    --readFilesIn $smp1 $smp2 \
+    --sjdbOverhang $ohang \
+    --sjdbGTFfile $ant \
+    --outFilterMultimapNmax 10 \
+    --outFilterMismatchNmax $mis \
+    --outSAMtype BAM SortedByCoordinate
 fi
+cd
 
+wait
 
 #sorts the accepted hits
-echo "sorting..."
+echo "[$smpkey] sorting..."
 samtools sort \
-    -n $out/accepted_hits.bam \
+    -n $out/Aligned.sortedByCoord.out.bam \
     -o $out/sort_accepted.bam
-echo "finished sorting"
+echo "[$smpkey] finished sorting"
 echo ""
 
+wait
+
 #filter the accepted hits by uniqueness
-echo "filter unique..."
+echo "[$smpkey] filter unique..."
 samtools view \
     -h $out/sort_accepted.bam \
     | perl $sort 1 \
     | samtools view -bS - \
     | samtools sort \
     -o $out/unique.bam
-echo "finished filtering"
+echo "[$smpkey] finished filtering"
 echo ""
 
+wait
+
 #adds read groups using picard, note the RG arguments are disregarded here
-echo "adding/replacing read groups..."
-java -jar /usr/bin/picard/picard.jar AddOrReplaceReadGroups \
+echo "[$smpkey] adding/replacing read groups..."
+gatk AddOrReplaceReadGroups \
     I=$out/unique.bam \
     O=$out/unique_RG.bam \
     RGID=1 \
@@ -187,46 +209,65 @@ java -jar /usr/bin/picard/picard.jar AddOrReplaceReadGroups \
     RGPL=illumina_100se \
     RGPU=HWI-ST1395:97:d29b4acxx:8 \
     RGSM=sample
-echo "finished adding/replacing read groups"
+echo "[$smpkey] finished adding/replacing read groups"
 echo ""
 
+wait
+
+if [ ! -e "$rout/ref/picard_ref.dict" ] 
+then 
+    gatk CreateSequenceDictionary \
+    R=$gno \
+    O=$rout/ref/picard_ref.dict
+    echo "Picard genome dictionary created"
+fi
+
+wait
+
 #reorder the reads using picard
-echo "reordering..."
-java -Xmx2g -Djava.io.tmpdir=$out/tmp \
-    -jar /usr/bin/picard/picard.jar ReorderSam \
+echo "[$smpkey] reordering..."
+gatk --java-options "-Xmx2g -Djava.io.tmpdir=$out/tmp" ReorderSam \
     I=$out/unique_RG.bam \
     O=$out/unique_RG_ordered.bam \
     R=$gno \
     CREATE_INDEX=TRUE \
+    SEQUENCE_DICTIONARY=$rout/ref/picard_ref.dict \
     TMP_DIR=$out/tmp
-echo "finished reordering"
+echo "[$smpkey] finished reordering"
 echo ""
+
+wait
 
 #splitting and cigarring the reads, using genome analysis tool kit
 #note can alter arguments to allow cigar reads 
-echo "getting split and cigar reads..."
-/Data04/harrli02/repo/gatk-4.3.0.0/gatk SplitNCigarReads \
+echo "[$smpkey] getting split and cigar reads..."
+gatk --java-options "-Xmx2g -Djava.io.tmpdir=$out/tmp" SplitNCigarReads \
     -R $gno \
     -I $out/unique_RG_ordered.bam \
-    -O $out/unique_RG_ordered_splitN.bam
-    #  -U ALLOW＃_N_CIGAR_READS
-echo "finished splitting N cigarring"
+    -O $out/unique_RG_ordered_splitN.bam \
+    # -U ALLOW_N_CIGAR_READS
+echo "[$smpkey] finished splitting N cigarring"
 echo ""
 
+wait
+
 #final resorting using picard
-echo "resorting..."
-java -Xmx2g -Djava.io.tmpdir=$out/tmp  \
-    -jar /usr/bin/picard/picard.jar SortSam \
+echo "[$smpkey] resorting..."
+gatk --java-options "-Xmx2g -Djava.io.tmpdir=$out/tmp" SortSam \
     I=$out/unique_RG_ordered_splitN.bam \
     O=$out/unique_RG_ordered_splitN.resort.bam \
     SORT_ORDER=coordinate
-echo "finished resorting"
+echo "[$smpkey] finished resorting"
 echo ""
 
+wait
+
 #hamr step, can take ~1hr
-echo "hamr..."
-python2.7 /Data04/harrli02/repo/HAMR/hamr.py \
-    -fe $out/unique_RG_ordered_splitN.resort.bam $gno $mdl $out $smpname 30 50 0.01 H4 .01 0 .05
+echo "[$smpkey] hamr..."
+python $hamr/hamr.py \
+    -fe $out/unique_RG_ordered_splitN.resort.bam $gno $mdl $out $smpname 30 50 0.01 H4 1 .05 .05
+
+wait
 
 if [ ! -e "$out/${smpname}.mods.txt" ]
 then 
@@ -237,3 +278,7 @@ else
 # HAMR needs separate folders to store temp for each sample, so we move at the end
     cp $out/${smpname}.mods.txt $hamrout
 fi
+
+# Move the unique_RG_ordered.bam and unique_RG_ordered.bai to a folder for read depth analysis
+cp $out/unique_RG_ordered.bam $rout/pipeline/depth/$smpname.bam
+cp $out/unique_RG_ordered.bai $rout/pipeline/depth/$smpname.bai
